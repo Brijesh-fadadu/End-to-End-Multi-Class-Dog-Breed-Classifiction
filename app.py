243df784232333d5246e202ab2fff4acc02335ca
Brijesh Fadadu <95677574+Brijesh-fadadu@users.noreply.github.com>
2022-09-25 17:36:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,132 @@
+import os
+import uuid
+import flask
+import urllib
+from PIL import Image
+from keras.models import load_model
+from flask import Flask , render_template  , request , send_file
+from keras.utils import load_img , img_to_array
+import tensorflow_hub as hub
+
+app = Flask(__name__)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = load_model(os.path.join(BASE_DIR , 'multi_class_dog.h5'),custom_objects={"KerasLayer":hub.KerasLayer})
+
+
+ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXT
+
+classes = ['affenpinscher', 'afghan_hound', 'african_hunting_dog', 'airedale',
+       'american_staffordshire_terrier', 'appenzeller',
+       'australian_terrier', 'basenji', 'basset', 'beagle',
+       'bedlington_terrier', 'bernese_mountain_dog',
+       'black-and-tan_coonhound', 'blenheim_spaniel', 'bloodhound',
+       'bluetick', 'border_collie', 'border_terrier', 'borzoi',
+       'boston_bull', 'bouvier_des_flandres', 'boxer',
+       'brabancon_griffon', 'briard', 'brittany_spaniel', 'bull_mastiff',
+       'cairn', 'cardigan', 'chesapeake_bay_retriever', 'chihuahua',
+       'chow', 'clumber', 'cocker_spaniel', 'collie',
+       'curly-coated_retriever', 'dandie_dinmont', 'dhole', 'dingo',
+       'doberman', 'english_foxhound', 'english_setter',
+       'english_springer', 'entlebucher', 'eskimo_dog',
+       'flat-coated_retriever', 'french_bulldog', 'german_shepherd',
+       'german_short-haired_pointer', 'giant_schnauzer',
+       'golden_retriever', 'gordon_setter', 'great_dane',
+       'great_pyrenees', 'greater_swiss_mountain_dog', 'groenendael',
+       'ibizan_hound', 'irish_setter', 'irish_terrier',
+       'irish_water_spaniel', 'irish_wolfhound', 'italian_greyhound',
+       'japanese_spaniel', 'keeshond', 'kelpie', 'kerry_blue_terrier',
+       'komondor', 'kuvasz', 'labrador_retriever', 'lakeland_terrier',
+       'leonberg', 'lhasa', 'malamute', 'malinois', 'maltese_dog',
+       'mexican_hairless', 'miniature_pinscher', 'miniature_poodle',
+       'miniature_schnauzer', 'newfoundland', 'norfolk_terrier',
+       'norwegian_elkhound', 'norwich_terrier', 'old_english_sheepdog',
+       'otterhound', 'papillon', 'pekinese', 'pembroke', 'pomeranian',
+       'pug', 'redbone', 'rhodesian_ridgeback', 'rottweiler',
+       'saint_bernard', 'saluki', 'samoyed', 'schipperke',
+       'scotch_terrier', 'scottish_deerhound', 'sealyham_terrier',
+       'shetland_sheepdog', 'shih-tzu', 'siberian_husky', 'silky_terrier',
+       'soft-coated_wheaten_terrier', 'staffordshire_bullterrier',
+       'standard_poodle', 'standard_schnauzer', 'sussex_spaniel',
+       'tibetan_mastiff', 'tibetan_terrier', 'toy_poodle', 'toy_terrier',
+       'vizsla', 'walker_hound', 'weimaraner', 'welsh_springer_spaniel',
+       'west_highland_white_terrier', 'whippet',
+       'wire-haired_fox_terrier', 'yorkshire_terrier']
+
+
+def predict(filename , model):
+    img = load_img(filename , target_size = (224 , 224))
+    img = img_to_array(img)
+    img = img.reshape(1,224,224,3)
+
+    img = img.astype('float32')
+    img = img/255.0
+    result = model.predict(img)
+
+    dict_result = {}
+    for i in range(120):
+        dict_result[result[0][i]] = classes[i]
+
+    res = result[0]
+    res.sort()
+    res = res[::-1]
+    prob = res[:3]
+    
+    prob_result = []
+    class_result = []
+    for i in range(3):
+        prob_result.append((prob[i]*100).round(2))
+        class_result.append(dict_result[prob[i]])
+
+    return class_result , prob_result
+
+
+@app.route('/')
+def home():
+        return render_template("index.html")
+
+@app.route('/prediction')
+def pred_page():
+        return render_template("prediction.html")
+
+@app.route('/success' , methods = ['GET' , 'POST'])
+def success():
+    error = ''
+    target_img = os.path.join(os.getcwd() , 'static/images')
+    if request.method == 'POST':
+
+        if (request.files):
+            file = request.files['file']
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(target_img , file.filename))
+                img_path = os.path.join(target_img , file.filename)
+                img = file.filename
+
+                class_result , prob_result = predict(img_path , model)
+
+                predictions = {
+                      "class1":class_result[0],
+                        "class2":class_result[1],
+                        "class3":class_result[2],
+                        "prob1": prob_result[0],
+                        "prob2": prob_result[1],
+                        "prob3": prob_result[2],
+                }
+
+            else:
+                error = "Please upload images of jpg , jpeg and png extension only"
+
+            if(len(error) == 0):
+                return  render_template('success.html' , img  = img , predictions = predictions)
+            else:
+                return render_template('prediction.html' , error = error)
+
+    else:
+        return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
